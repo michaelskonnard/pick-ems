daca4e7a12d10e6afecc6b94ff77840d246cd3f9
michaelskonnard <michaelskonnard@gmail.com>
2023-11-14 18:10:46 -0700

    fixes build error

--- pick-ems-frontend/src/app/page.tsx
+++ pick-ems-frontend/src/app/page.tsx
@@ -71,7 +71,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            If you are'nt Lij, gtfo.
+            If you are{'\''}nt Lij, gtfo.
           </p>
         </a>
       </div>
