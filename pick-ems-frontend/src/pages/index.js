40a381f818aeaff89e0a79490cacf1ae5adaa940
michaelskonnard <michaelskonnard@gmail.com>
2024-05-15 10:35:31 -0600

    adds football icon

--- pick-ems-frontend/src/pages/index.js
+++ pick-ems-frontend/src/pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
           <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
             <div className="text-center">
               <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                Pickems
+                Pickems 🏈
               </h1>
               <p className="mt-6 text-lg leading-8 text-gray-600">
                 The Ultimate Sports Prediction Game with Friends!
